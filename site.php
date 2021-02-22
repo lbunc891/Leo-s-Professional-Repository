9cffa08d32541aa746f0e914ce82f41b6417fa64
Leo Bunczak <lbunc891@uwsp.edu>
2021-02-21 21:04:17 -0600

    Added site.php

---
+++ site.php
@@ -0,0 +1,14 @@
+<?php
+print '<!doctype html>';
+print '<html>';
+print '<head>';
+print '<meta charset="utf-8">';
+print '<title>Untitled Document</title>';
+print '<link href="style.css" rel="stylesheet" type="text/css">';
+print '<script src="hello.js"></script>';
+print '</head>';	
+print '<body>';
+print '<h1>Leo Bunczak</h1>';
+print '</body>';
+print '</html>';
+?>
