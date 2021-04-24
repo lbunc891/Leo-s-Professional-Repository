f9e92e24c8dfba180ef7e3ac63cd975d29c8e5b5
Leo Bunczak <lbunc891@uwsp.edu>
2021-04-23 22:24:59 -0500

    Added a question page and a home page

---
+++ week_12/home.php
@@ -0,0 +1,28 @@
+<?php
+require_once("includes.php");
+
+if(!isset($_SESSION['isLoggedIn']) || $_SESSION['isLoggedIn'] === false)
+{
+	die(header("Location: " . LOGIN_PAGE));
+}
+
+require_once("SiteTemplate.php");
+
+$page = new SiteTemplate("LoginPage");
+$page->addHeadElement('<link href="home.css" rel="stylesheet" type="text/css">');
+$page->finalizeTopSection();
+$page->finalizeBottomSection();
+print $page->getTopSection();
+
+print'<body class="d-flex h-100 text-center text-white bg-dark">';
+print'<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">';
+print'<header class="mb-auto">';
+print'<div>';
+print'<nav class="nav nav-masthead justify-content-center float-md-end">';
+print'<a class="nav-link active" aria-current="page" href="logout.php">Log Out</a>';
+print'</nav>';
+print'</div>';
+print'</header>';
+print'<main class="px-3">';
+print'<h1>Welcome, ' . $_SESSION['username'] . '</h1>';
+print'</main>';
---
+++ week_12/users.php
@@ -0,0 +1,4 @@
+<?php
+
+$users = array("Leo" => '$2y$10$tK9aRjCMv9EW58D56xgpoegO2MeeB/PYySWdjq/5USF.bManxatiy', 
+			   "Bunczak" => '$2y$10$TokiNUSQ6eQTXgTUK4n14.bqql0YDIhq0Wuj/qy7W.h.X5AEMUajS');
